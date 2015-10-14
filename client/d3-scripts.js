8903ac7b6f8109ca4a53252b51b72da60e9e3a5a
RyanYocum <RyanCYocum@gmail.com>
2015-10-13 17:22:19 -0700

    created a line chart with axes

--- client/d3-scripts.js
+++ client/d3-scripts.js
@@ -2,14 +2,16 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
+var x0 = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
 
 var y = d3.scale.linear()
     .range([height, 0]);
 
 var xAxis = d3.svg.axis()
-    .scale(x)
+    .scale(x0)
     .orient("bottom");
 
 var yAxis = d3.svg.axis()
@@ -24,19 +26,22 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-var data1 = $.ajax({
-    url: "http://novlovplov.xyz/api/accounts/",
-    datatype: "json",
-    success: function(){
-      var data2 = data1.responseJSON;
-      data = [];
+// var data1 = $.ajax({
+//     url: "http://novlovplov.xyz/api/accounts/",
+//     datatype: "json",
+//     success: function(){
+//       var data2 = data1.responseJSON;
+//       data = [];
       
-      for (var i = 0; i < data2.length; i++) {
-        data.push({value: data2[i].currentMMR, name: data2[i].username});
-      }
+//       for (var i = 0; i < data2.length; i++) {
+//         data.push({current: data2[i].currentMMR, name: data2[i].username, original: data2[i].startingMMR});
+//       }
 
-       x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+d3.json('http://novlovplov.xyz/api/accounts/', function (error, data) {
+  var MMRs = d3.keys(["startingMMR", "currentMMR"]);
+       x0.domain(data.map(function(d) { return d.name; }));
+       x1.domain(MMRs).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(d) { return d.current; })]);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -51,20 +56,20 @@ var data1 = $.ajax({
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("value");
+      .text("current");
 
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
-    }
+      .attr("x0", function(d) { return x0(d.name); })
+      .attr("width", x0.rangeBand())
+      .attr("y", function(d) { return y(d.currentMMR); })
+      .attr("height", function(d) { return height - y(d.function); });
+//     }
 });
 
-function type(d) {
-  d.value = +d.value; // coerce to number
+currentMMR type(d) {
+  d.current = +d.current; // coerce to number
   return d;
 }
